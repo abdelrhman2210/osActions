6728f717d62a03b7bd5a1d0cf3a16f519d9babc9
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2024-11-28 23:31:40 +0200

    act

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-Binary search, Two pointers
-Binary Search
-Number Theory: GCD && LCM && Divisibility
-Bitwise operators & complete search (Bitmasking)
-Number Theory: Prime and Composite, Primality Testing, Prime Sieves, Finding Divisors
-Week 3: prefix sum & partial sum
-Week 2: STL (map, set, priority_queue, multi set, unordered map)
-Week 1: STL (vector , pair , stack , queue , deque, struct)
-bool fi
+class Main{
+    public static void main(String[]args){
+        System.out.println("abdelrahman essam");
+    }
+}
